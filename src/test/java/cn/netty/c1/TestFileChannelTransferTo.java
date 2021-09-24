584e1628a15d61534f4ed0a6d1857277ce81f521
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-24 10:01:16 +0800

    FileChannel传输数据大于2g

---
+++ src/test/java/cn/netty/c1/TestFileChannelTransferTo.java
@@ -0,0 +1,26 @@
+package cn.netty.c1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class TestFileChannelTransferTo {
+    public static void main(String[] args) {
+        try (FileChannel from = new FileInputStream("data.txt").getChannel();
+             FileChannel to = new FileOutputStream("to.txt").getChannel();
+        ) {
+            // 效率高,底层会利用操作系统的零拷贝进行优化,但传输数据每次最大2g
+//            from.transferTo(0, from.size(), to);
+            long size = from.size();
+            //left 变量代表还剩余多少字节
+            for (long left = size;left > 0;) {
+                System.out.println("position:" + (size - left) + " left:" + left);
+                left -= from.transferTo((size - left), left, to);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
