168d8033f3af694cc37f5e52fa3e6c1c48fc9c05
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-24 11:05:18 +0800

    files-walk拷贝多级目录

---
+++ src/test/java/cn/netty/c1/TestFilesCopy.java
@@ -0,0 +1,28 @@
+package cn.netty.c1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestFilesCopy {
+    public static void main(String[] args) throws IOException {
+        String source = "D:\\ico";
+        String target = "D:\\ico_copy";
+
+        Files.walk(Paths.get(source)).forEach(path -> {
+            try {
+                String targetName = path.toString().replace(source, target);
+                // 是目录
+                if (Files.isDirectory(path)) {
+                    Files.createDirectory(Paths.get(targetName));
+                }
+                // 是普通文件
+                else if (Files.isRegularFile(path)) {
+                    Files.copy(path, Paths.get(targetName));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
