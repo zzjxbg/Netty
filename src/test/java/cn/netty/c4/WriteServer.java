697d1ff15accd1d4ce32653daabc39b50739f857
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-27 10:05:26 +0800

    nio-selector-处理可写事件

--- src/test/java/cn/netty/c4/WriteServer.java
+++ src/test/java/cn/netty/c4/WriteServer.java
@@ -14,29 +14,48 @@ public class WriteServer {
     public static void main(String[] args) throws IOException {
         ServerSocketChannel ssc = ServerSocketChannel.open();
         ssc.configureBlocking(false);
+        ssc.bind(new InetSocketAddress(8080));
 
         Selector selector = Selector.open();
         ssc.register(selector, SelectionKey.OP_ACCEPT);
 
-        ssc.bind(new InetSocketAddress(8080));
-
         while(true) {
             selector.select();
+
             Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
-            while(iter.hasNext()) {
+            while (iter.hasNext()) {
                 SelectionKey key = iter.next();
-                SocketChannel sc = ssc.accept();
-                sc.configureBlocking(false);
-                //1.向客户端发送大量数据
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0;i < 30000000;i++) {
-                    sb.append("a");
-                }
-                ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
-                while (buffer.hasRemaining()) {
-                    //2.返回值代表实际写入的字节数
+                iter.remove();
+                if (key.isAcceptable()) {
+                    SocketChannel sc = ssc.accept();
+                    sc.configureBlocking(false);
+                    SelectionKey sckey = sc.register(selector, SelectionKey.OP_READ);
+                    // 1. 向客户端发送内容
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 3000000; i++) {
+                        sb.append("a");
+                    }
+                    ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
                     int write = sc.write(buffer);
-                    System.out.println(write);
+                    // 3. write 表示实际写了多少字节
+                    System.out.println("实际写入字节:" + write);
+                    // 4. 如果有剩余未读字节，才需要关注写事件
+                    if (buffer.hasRemaining()) {
+                        // read 1  write 4
+                        // 在原有关注事件的基础上，多关注 写事件
+                        sckey.interestOps(sckey.interestOps() + SelectionKey.OP_WRITE);
+                        // 把 buffer 作为附件加入 sckey
+                        sckey.attach(buffer);
+                    }
+                } else if (key.isWritable()) {
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    int write = sc.write(buffer);
+                    System.out.println("实际写入字节:" + write);
+                    if (!buffer.hasRemaining()) { // 写完了
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE); //不需关注可写事件
+                        key.attach(null);
+                    }
                 }
             }
         }
