eaefcfc9fe725d9226de03638f328e1e8655c017
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-29 13:45:24 +0800

    netty入门-ChannelFuture-处理结果

--- src/test/java/cn/netty/c3/EventLoopClient_ChannelFuture.java
+++ src/test/java/cn/netty/c3/EventLoopClient_ChannelFuture.java
@@ -3,6 +3,7 @@ package cn.netty.c3;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -14,6 +15,7 @@ import java.net.InetSocketAddress;
 @Slf4j
 public class EventLoopClient_ChannelFuture {
     public static void main(String[] args) throws InterruptedException {
+        // 2.带有Future、Promise的类型都是和异步方法配套使用,用来处理结果
         ChannelFuture channelFuture = new Bootstrap()
                 .group(new NioEventLoopGroup())
                 .channel(NioSocketChannel.class)
@@ -26,13 +28,25 @@ public class EventLoopClient_ChannelFuture {
                 // 1. 连接到服务器
                 // connect方法异步非阻塞,main方法发起调用,真正执行connect是nio线程
                 .connect(new InetSocketAddress("localhost",8080));  // 1s后连接上
-        channelFuture.sync();
+        // 2.1 使用sync方法同步处理结果
+//        channelFuture.sync(); //阻塞住当前线程(主线程),直到nio线程连接建立完毕
         // 无阻塞向下执行获取channel
         // connect 方法是异步的,意味着不等连接建立,方法执行就返回了
         // 因此channelFuture对象中不能立刻获得到正确的Channel对象
-        Channel channel = channelFuture.channel();
-        log.debug("{}",channel);
-        // 2.向服务器发送数据
-        channel.writeAndFlush("success");
+//        Channel channel = channelFuture.channel();
+//        log.debug("{}",channel);
+//        channel.writeAndFlush("success");
+
+        // 2.2 使用addListener(回调对象) 方法异步处理结果(主线程交由其他线程完成连接后调用)
+        channelFuture.addListener(new ChannelFutureListener() {
+            @Override
+            // 在nio线程连接建立好之后,会调用operationComplete
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                Channel channel = channelFuture.channel();
+                log.debug("{}",channel);
+                channel.writeAndFlush("success");
+            }
+        });
+
     }
 }
