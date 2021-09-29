4dfc0ddc96146e2061b871597ffdb37591f0a57f
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-29 14:36:37 +0800

    netty入门-ChannelFuture-处理关闭

--- src/test/java/cn/netty/c3/CloseFutureClient.java
+++ src/test/java/cn/netty/c3/CloseFutureClient.java
@@ -3,10 +3,13 @@ package cn.netty.c3;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
@@ -15,12 +18,14 @@ import java.util.Scanner;
 @Slf4j
 public class CloseFutureClient {
     public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
         ChannelFuture channelFuture = new Bootstrap()
-                .group(new NioEventLoopGroup())
+                .group(group)
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         ch.pipeline().addLast(new StringEncoder());
                     }
                 })
@@ -33,11 +38,28 @@ public class CloseFutureClient {
                 String line = scanner.nextLine();
                 if ("q".equals(line)) {
                     channel.close();  // close异步操作 1s之后
+//                    log.debug("处理关闭之后的操作");
                     break;
                 }
                 channel.writeAndFlush(line);
             }
         },"input").start();
-        log.debug("处理关闭之后的操作");
+
+        // 获取ClosedFuture对象
+        ChannelFuture closeFuture = channel.closeFuture();
+
+        // 1)同步处理关闭(主线程阻塞,直到nio线程连接建立完毕)
+//        System.out.println("waiting close...");
+//        closeFuture.sync();
+//        log.debug("处理关闭之后的操作");
+
+        // 2)异步处理关闭(主线程交由关闭线程完成打印)
+        closeFuture.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                log.debug("处理关闭之后的操作");
+                group.shutdownGracefully(); // 关闭EventLoopGroup,彻底关闭java程序
+            }
+        });
     }
 }
