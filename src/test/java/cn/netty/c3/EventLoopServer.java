4f2b3024aee2ed8d580c5eeb8bacae81bd6e116d
Zijie Zhao <Zijie.Zhao@cicc.com.cn>
2021-09-29 11:01:30 +0800

    netty入门-eventloop-切换线程

--- src/test/java/cn/netty/c3/EventLoopServer.java
+++ src/test/java/cn/netty/c3/EventLoopServer.java
@@ -30,6 +30,10 @@ public class EventLoopServer {
                                 log.debug(buf.toString(Charset.defaultCharset()));
                                 ctx.fireChannelRead(msg);  //将消息传递给下一个handler
                             }
+                            /**
+                             * 如果两个handler绑定的是同一个线程,那么就直接调用.否则,把要调用的代码
+                             * 封装为一个任务对象,由下一个handler的线程来调用
+                             */
                         }).addLast(group, "handler2", new ChannelInboundHandlerAdapter() {
                             @Override                                           //ByteBuf
                             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
